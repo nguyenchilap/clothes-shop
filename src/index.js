d15f4a0f540238749f47f27e04e4aeba5e83509c
LẬP NGUYỄN CHÍ <18120435@student.hcmus.edu.vn>
2022-06-22 19:50:12 +0700

    deploy

--- src/index.js
+++ src/index.js
@@ -32,6 +32,7 @@ app.use(cors(corsOption));
 //Route init
 app.use('/api', baseRouter);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const server = app.listen(process.env.PORT || 5000, () => {
+  const port = server.address().port;
+  console.log(`Express is working on port ${port}`);
+});
